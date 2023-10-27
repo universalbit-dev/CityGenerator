6fdd0f1b08d58a69c8e6cb3339f59039a131e2a3
UniversalBit <31393557+universalbit-dev@users.noreply.github.com>
2023-10-27 12:56:35 +0200

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,23 @@
 const path = require('path');
 
-module.exports = {
-  entry: './src/main.ts',
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-    ],
-  },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
-  },
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-};
+  module.exports = {
+    entry: './src/index.ts',
+
+   devtool: 'inline-source-map',
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: 'ts-loader',
+          exclude: /node_modules/,
+        },
+      ],
+    },
+    resolve: {
+      extensions: [ '.tsx', '.ts', '.js' ],
+    },
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+  };
