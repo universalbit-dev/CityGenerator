fcf3ab70737b5745ccd53648367fa245306f0c43
UniversalBit <lazzaragiuliano@gmail.com>
2025-01-24 01:26:54 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const express = require('express');const path = require('path');
 
 const app = express();
-const port = 8080;
-
+const port = process.env.PORT || 8080;
 app.use(express.static(path.join(__dirname, "dist/")))
 
 app.listen(port);
