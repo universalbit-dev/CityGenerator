c1850a8d87be0287b4205646f511d400f8deee5f
UniversalBit <lazzaragiuliano@gmail.com>
2025-05-10 00:57:38 +0200

    Add files via upload

--- webpack.config.js
+++ webpack.config.js
@@ -71,14 +71,11 @@ module.exports = {
     {
       test: /\.json$/,
       type: 'json',
-    },
-    {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
     }
   ]
 },
 
+  
   resolve: {
     extensions: ['.ts', '.js' ,'.json','.d.ts','.dt.ts'],
     byDependency: {
