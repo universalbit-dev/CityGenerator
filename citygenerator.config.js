01c9673ebbd9f69f4eb38116f18e5bfd8e710cb9
UniversalBit <31393557+universalbit-dev@users.noreply.github.com>
2026-07-04 11:46:18 +0200

    Add configuration options for instances and timeouts

--- citygenerator.config.js
+++ citygenerator.config.js
@@ -4,6 +4,10 @@ module.exports = {
       name: "CityGenerator",
       script: "./https_server.js",
       exec_mode: "fork",
+      instances: 1,
+      max_memory_restart: "1G",
+      listen_timeout: 8000,
+      kill_timeout: 5000,
       env: {
         NODE_ENV: "production"
       }
